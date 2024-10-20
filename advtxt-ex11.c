c557821b920659980d5b5c1f06b4fcd40e75749d
Gabriel Henrique De Lima <ghl@poli.br>
2024-10-20 19:00:47 -0300

    adicionei a ultima parte do codigo ex11.c

--- advtxt-ex11.c
+++ advtxt-ex11.c
@@ -39,4 +39,46 @@ int main()
                 else
                     if(j != 1 && j != 2)
                         printf("digite um valor aceitavel,\n 1\n ou\n 2\n\n");
-            }
+            }
+            switch(j)
+            {
+                case 1:
+                    printf("voces conseguiram uma amostra do sangue de morbius, podera ser usado para fazer um antidoto contra o vampiro!!!\n");
+                    printf("um de seus amigos eh cientista e consegue fazer um antidoto\n");
+                    printf("Todos escutam um forte barulho vindo de fora\n\n");
+                    printf("1. voces saem para descobrir o que aconteceu\n");
+                    printf("2. ficar e ajudar com o antidoto\n\n");
+                    while(k != 1 && k != 2)
+                    {
+                        if(scanf("%i", &k) != 1)
+                        {
+                            printf("digite um valor aceitavel,\n 1\n ou\n 2\n\n");
+                            while(getchar() != '\n');
+                            k = 0;/* limpa o buffer, caso digite uma entrada invalida e o retorna ao valor padrao */
+                        }
+                        else
+                            if(k != 1 && k != 2)
+                                printf("digite um valor aceitavel,\n 1\n ou\n 2\n\n");
+                    }
+                    switch(k)
+                    {
+                        case 1:
+                            printf("Morbius aparece e ataca todos voces.\n Infelizmente Voce e seu grupo morreram tendo seus sangues sugados por Morbius!!!\n\n GAME OVER!\n\n");
+                            break;
+                        case 2:
+                            printf("Morbius invade o prédio e fareja todos voces!\n Por sorte, seu amigo consegue terminar o antidoto!\n Morbius os ataca, mas voces atiram o antidoto nele.\n Parabens, Voces salvaram a cidade de Nova York!\n\n Voce Venceu!\n\n");
+                            break;
+                    }
+                    break;
+                case 2:
+                    printf("voce achou uma estaca e acreditou que estava vivendo em um dos contos de vampiros comuns.\n\n Morbius te encontrou!\n\n ao usar sua estaca no vampiro, voce tem o azar de descobrir que ele resistiu!\n voce morreu e seu grupo tambem!\n\n GAME OVER\n\n");
+                    break;
+            }
+
+            break;
+        case 2:
+            printf("até uma proxima!\n");
+            break;
+    }
+    return 0;
+}
